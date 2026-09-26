d8ba74acd0235c1a4c29465f6bebe806c11089cd
Tonmoy Paul <snahasishpaul7@gmail.com>
2026-09-26 17:22:33 +0600

    Finalize the pandas sorting example
    Removed unnecessary line breaks and fixed formatting.

--- sorting.py
+++ sorting.py
@@ -2,31 +2,20 @@ import pandas as pd
 
 data = {
     "ID": [1005, 1002, 1008, 1001, 1006, 1003],
-
     "Name": [
         "Hasan",
         "Karim",
         "Nayeem",
         "Rahim",
         "Sakib",
-        "Tanvir"
+        "Tanvir",
     ],
-
-    "Age": [
-        25,
-        21,
-        24,
-        20,
-        23,
-        22
-    ]
+    "Age": [25, 21, 24, 20, 23, 22],
 }
 
 df = pd.DataFrame(data)
 
 print(df)
-df.sort_values(by="Age",ascending=True,inplace=True)
-
-df.sort_values(by=["Age","ID"],ascending=True,inplace=True)
-
+df.sort_values(by="Age", ascending=True, inplace=True)
+df.sort_values(by=["Age", "ID"], ascending=True, inplace=True)
 print(df)
